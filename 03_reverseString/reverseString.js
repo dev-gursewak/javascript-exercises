3980f9c0cff560afa7a61cb348603c70f18fb877
dev-gursewak <gursewak@duck.com>
2024-09-22 17:21:48 -0600

    completed the reversing string as well

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let stringArray = [];
+    for (string of inputString){
+        stringArray.push(string);
+    }
+    stringArray.reverse();
+    return stringArray.join('');
 };
 
 // Do not edit below this line
