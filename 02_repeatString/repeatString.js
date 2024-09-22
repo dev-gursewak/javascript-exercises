a461c76c22aff9926964ed68d5de306f4bdce4ff
dev-gursewak <gursewak@duck.com>
2024-09-22 17:07:18 -0600

    completed the first problem other that hello world
    it's done with if and for

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, noOfTimesRepeating) {
+    let resultString = '';
+    if (noOfTimesRepeating<0){
+        return "ERROR"
+    } else {
+        for (i=1; i<=noOfTimesRepeating; i++){
+            resultString = resultString.concat('', inputString);
+    }  return resultString;
+}
+    
 };
 
 // Do not edit below this line
