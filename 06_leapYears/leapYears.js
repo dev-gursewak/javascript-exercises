76c5e19f7df261660a86e814c7acfc4c5f544f1e
dev-gursewak <gursewak@duck.com>
2024-09-22 18:45:52 -0600

    completed tempConversion

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if (inputYear%4 == 0 && inputYear%25 != 0 ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
