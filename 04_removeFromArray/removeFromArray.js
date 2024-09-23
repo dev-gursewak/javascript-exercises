ffe0d743e592635caca48d93c7178a1e9e6008bd
dev-gursewak <gursewak@duck.com>
2024-09-22 18:21:13 -0600

    compelted sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, valueToRemove) {
+    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(startingValue, endingValue) {
+    let sum = 0;
+    if (Math.sign(startingValue)==-1 ||
+        Math.sign(endingValue)==-1 ||
+        typeof startingValue == "string" || 
+        typeof endingValue == "string" ||
+        Math.floor(startingValue) != startingValue ||
+        Math.floor(endingValue) != endingValue) {
+            return "ERROR"
+        
+    } else if (startingValue < endingValue) {
+        for (i=startingValue; i<=endingValue; i++){
+            sum += i;
+            }
+            return sum;
+    } else if (startingValue > endingValue){
+        for (i=endingValue; i<=startingValue; i++){
+            sum +=i;
+        } return sum;
+    }
 };
 
 // Do not edit below this line
